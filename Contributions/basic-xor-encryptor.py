eb6228f8665a2aa2177be7dec8e56303e6c4db1e
Vishnu Sudhakaran <79243096+an0n4ce@users.noreply.github.com>
2021-10-03 10:31:53 +0530

    Create basic-xor-encryptor.py

---
+++ Contributions/basic-xor-encryptor.py
@@ -0,0 +1,30 @@
+text = "flag{be551b494eac9a69a4584b8e2db0bb20}"
+
+def encrypt(text,key):
+    encrypted_txt = ''
+    hex_enc_txt = ''
+    for i in range(0,len(text)):
+        xored = ord(text[i]) ^ ord(key)
+        encrypted_txt +=  chr(xored)
+        hex_enc_txt += hex(xored) + '-'
+    li = list(hex_enc_txt)
+    li.pop()
+    hex_enc_txt = ''.join(li)
+    return encrypted_txt,hex_enc_txt
+
+def decrypt(ciphered_txt,key):
+    decrypted_txt = ''
+    for i in range(0,len(ciphered_txt)):
+        xored = ord(ciphered_txt[i]) ^ ord(key)
+        decrypted_txt += chr(xored)
+    return decrypted_txt
+
+alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+for i in alpha:
+    ciphered_txt,cip_hex = encrypt(text, 'Q')
+
+    # print(ciphered_txt)
+    print(cip_hex)
+
+    deciphered_txt = decrypt(ciphered_txt, i)
+    print(deciphered_txt)
