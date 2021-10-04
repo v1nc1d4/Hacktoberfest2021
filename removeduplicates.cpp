5fe6558d1701fe24d66e298ebb07c2a7fb93c159
B Lekheshwar <66664839+Lekheshwar@users.noreply.github.com>
2021-10-05 01:26:24 +0530

    Create removeDuplicates.cpp

---
+++ removeduplicates.cpp
@@ -0,0 +1,15 @@
+int removeDuplicates(vector<int>& nums) {
+	int n = nums.size();
+	if (n == 0)return 0;
+	int i = 0, j = 1;
+
+	while (j < n) {
+		if (nums[i] != nums[j]) {
+			nums[++i] = nums[j++];
+		}
+		else {
+			j++;
+		}
+	}
+	return i + 1;
+}
